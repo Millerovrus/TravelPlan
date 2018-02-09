4177b0c854735591ed6e4f04794685cb70fcc269
foo <pol6.64@mail.ru>
2018-02-09 09:35:24 +0300

    Deleted

--- TravelPlanner/src/main/java/com/netcracker/travelplanner/entities/RouteType.java
+++
@@ -1,5 +0,0 @@
-package com.netcracker.travelplanner.entities;
-
-public enum RouteType {
-    cheap, optimal, comfort
-}
--- travelplanner/src/main/resources/static/js/directives.js
+++
@@ -1,8 +0,0 @@
-import template from '../templates/menu.html';
-
-angular.module('myApp',[])
-    .directive('myMenu', function () {
-        return {
-            template: template
-        };
-    });
