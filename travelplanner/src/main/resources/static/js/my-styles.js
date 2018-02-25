f82ba7841bf33b94b23c215b352c9f1616145c6e
vishnyakovava <vishnyakova.valeria@gmail.com>
2018-02-25 15:47:18 +0300

    changed collapse routes

--- travelplanner/src/main/resources/static/js/my-styles.js
+++ travelplanner/src/main/resources/static/js/my-styles.js
@@ -77,9 +77,9 @@ $(function () {
 });
 
 /*collapse*/
-$(function(){
+/*$(function(){
     $('.card-head > span').click(function(){
         $(this).parent().parent().toggleClass('collapsed');
         $(this).parent().parent().find('.card-body').slideToggle();
     });
-});
+});*/
